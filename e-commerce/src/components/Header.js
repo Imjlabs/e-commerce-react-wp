a8ff7c9b93ad8d150744339330f8e8374b2a4fdd
Ghita-AMU <ghita.ajoubair@etu.univ-amu.fr>
2023-11-02 16:26:36 +0100

    changement du code post

--- e-commerce/src/components/Header.js
+++ e-commerce/src/components/Header.js
@@ -65,17 +65,27 @@ const Header = () => {
           <Suggestions suggestions={searchResults} handleSelect={handleSelectProduct} />
         )}
       </div>
-      <div className="header-links">
+      <div className="header-catalogue">
+        <div className="header-productList">
+        <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-grid" viewBox="0 0 16 16">
+            <path d="M3 4a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H3ZM3 9a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1v-1a1 1 0 0 0-1-1H3ZM8 4a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H8ZM8 9a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1v-1a1 1 0 0 0-1-1H8Z"/>
+          </svg></span>
+          <Link to="/Catalogue">Catalogue</Link>
+          </div>
         <div className="header-cart">
           <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
             <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/></svg>
-          </span> <Link to="/Panier">Panier</Link>
+          </span> 
+          <Link to="/Panier">Panier</Link>
         </div>
         <div className="header-profile">
           <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
             <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/></svg>
-          </span> <Link to="/Connexion">Profil</Link>
+          </span>
+           <Link to="/Connexion">Profil</Link>
         </div>
+        
+
       </div>
     </div>
   );
--- e-commerce/src/components/Post.js
+++ e-commerce/src/components/Post.js
@@ -7,9 +7,11 @@ var session_url = 'https://eisee-it.o3creative.fr/2023/groupe3/wp-json/wc/v3/pro
 var username = 'ck_e30e489bfe9990edb792ce1ad7436620dff7cb29';
 var password = 'cs_82c3e0ccfb784baa8052e1edfbc438aa3f3724fc';
 
-const Product = () => {
+const Post = () => {
   const { productId } = useParams();
   const [product, setProduct] = useState(null);
+  const [selectedProducts, setSelectedProducts] = useState([]);
+  
 
   useEffect(() => {
     const fetchData = async () => {
@@ -20,33 +22,58 @@ const Product = () => {
             password: password
           }
         });
-        console.log(response.data);
-        console.log(productId);
         setProduct(response.data);
       } catch (error) {
         console.error('Error fetching product data', error);
       }
     };
-
     fetchData();
   }, [productId]);
 
   if (!product) {
     return <p>Loading...</p>;
   }
+  const addToCart = (product) => {
+    if (selectedProducts.includes(product)) {
+      // Si le produit est déjà sélectionné, le désélectionner
+      setSelectedProducts(selectedProducts.filter((p) => p !== product));
+    } else {
+      // Sinon, l'ajouter à la liste des produits sélectionnés
+      setSelectedProducts([...selectedProducts, product]);
+    }
+  };
+
+  const isProductSelected = (product) => {
+    return selectedProducts.includes(product);
+  };
 
   return (
-    <div>
-      <h1>{ReactHtmlParser(product.name)}</h1>
-      <p>Prix : {ReactHtmlParser(product.price_html)}</p>
-      {product.images && product.images.length > 0 ? (
-        <img src={product.images[0].src} alt={product.name} width='20%' height='20%' />
-      ) : (
-        <p>Aucune image disponible</p>
-      )}
-      <button>Ajouter au panier</button>
+    <div className="product-page">
+      <div className="product-content">
+        <img className="product-imageP" src={product.images[0].src} alt={product.name} />
+        <div className="product-info">
+          <h1 className="product-title">{ReactHtmlParser(product.name)}</h1>
+          <p className="product-price">Prix : {ReactHtmlParser(product.price_html)}
+          </p>
+          <button 
+    onClick={() => {
+        addToCart(product);
+    }} 
+    className={`panier ${isProductSelected(product) ? "selected" : ""}`}>
+    {isProductSelected(product) ? "Produit ajouté au panier" : "Ajouter au panier"}
+</button>
+
+        </div>
+      </div>
+      <div className="product-details">
+        <p className='product-description'>Description : {ReactHtmlParser(product.description)}</p>
+        
+
+      </div>
     </div>
-  );
+);
+
+
 };
 
-export default Product;
+export default Post;
